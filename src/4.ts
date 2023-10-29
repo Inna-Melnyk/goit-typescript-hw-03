96e2f25ce8974ded3f890f9ac14a4ea114a8b2f1
Inna-Melnyk <inamelnyk@ukr.net>
2023-10-29 17:40:50 +0100

    completed 4 tasks
    completed 4 tasks

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,59 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature():number {
+    return this.signature;
+  }
+}
+
+
+class Person {
+  constructor(private key: Key) {}
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  abstract openDoor(key: Key): void;
+
+  public comeIn(person: Person) {
+    if (this.door) {
+      return this.tenants.push(person);
+    } else {
+      console.log("The door is closed");
+    }
+  }
+}
+
+class MyHouse extends House{
+
+    openDoor(key: Key): void {
+    if (key === this.key) {
+      this.door = true;
+      console.log('The door is open.');
+    } else {
+      this.door = false;
+      console.log('The door is closed. Invalid key.');
+    }
+  }
+    
+}
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +62,7 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
+
 
 
 export {};
